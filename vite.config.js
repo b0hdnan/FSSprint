fb51f802a7b8fd8665576753f5bc5f87a60d8cfa
b0hdnan <boplan1985@gmail.com>
2025-06-12 23:46:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-// vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: "/FSSprint/",
+  base: '/FSSprint/',
+  build: {
+    outDir: 'dist',
+  },
 });
